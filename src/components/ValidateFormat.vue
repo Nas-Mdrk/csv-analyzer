5c26729fb0de99c72e819bdbf63ea86ff912b9a0
Nas <120623658+Nas-Mdrk@users.noreply.github.com>
2024-09-23 13:15:34 +0300

    ValidateFormat.vue

--- src/components/ValidateFormat.vue
+++ src/components/ValidateFormat.vue
@@ -278,11 +278,11 @@ export default {
       formData.append('validation_rules', JSON.stringify(this.validationRules));
 
       try {
-        await axios.get('https://back-csv-analyzer-production.up.railway.app/', {
+        await axios.get('https://c0a44c3a82db72b42e1f39b65877118c.serveo.net/', {
         withCredentials: true
         });
         // Envoyer le fichier au backend
-        const response = await axios.post('https://back-csv-analyzer-production.up.railway.app/clean_and_validate_data', formData, {
+        const response = await axios.post('https://c0a44c3a82db72b42e1f39b65877118c.serveo.net/clean_and_validate_data', formData, {
           headers: {
             'Content-Type': 'multipart/form-data',
           },
@@ -294,14 +294,14 @@ export default {
         // Vérifiez périodiquement l'état de la tâche
         const interval = setInterval(async () => {
           try {
-            const statusResponse = await axios.get(`https://back-csv-analyzer-production.up.railway.app/task_status/${taskId}`, {
+            const statusResponse = await axios.get(`https://c0a44c3a82db72b42e1f39b65877118c.serveo.net/task_status/${taskId}`, {
               withCredentials: true, // Inclure les cookies
             });
             const status = statusResponse.data;
 
             if (status.state === 'SUCCESS') {
               clearInterval(interval);
-              this.downloadUrl = `https://back-csv-analyzer-production.up.railway.app/download_clean_csv/${taskId}`;
+              this.downloadUrl = `https://c0a44c3a82db72b42e1f39b65877118c.serveo.net/download_clean_csv/${taskId}`;
               this.taskStatus = 'Traitement terminé avec succès.';
               this.notification = null; // Hide notification
               this.isLoading = false; // Arrêter de charger

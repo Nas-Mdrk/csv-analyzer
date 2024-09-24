23f5d84b0ed0c10d9b3b6c3f55cdaf8b0a5db3a4
Nas <120623658+Nas-Mdrk@users.noreply.github.com>
2024-09-24 11:49:51 +0300

    StatistiqueDetail.vue

--- src/components/StatistiqueDetail.vue
+++ src/components/StatistiqueDetail.vue
@@ -204,12 +204,12 @@ export default {
     async updateTable() {
       try {
         // Première requête pour vérifier la connexion au backend
-        await axios.get('https://3538dde51d63156b011ebaa33232d05b.serveo.net/', {
+        await axios.get('back-csv-analyzer-production.up.railway.app/', {
           withCredentials: true 
       });
         this.errorMessage = ''; 
 
-        const response = await axios.get(`https://3538dde51d63156b011ebaa33232d05b.serveo.net/aggregated_data`, {
+        const response = await axios.get(`back-csv-analyzer-production.up.railway.app/aggregated_data`, {
           params: {
             month: this.month,
             year: this.year,

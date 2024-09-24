d8f96de105647982420c51ce0fde9f85e1d8574f
Nas <120623658+Nas-Mdrk@users.noreply.github.com>
2024-09-24 11:49:18 +0300

    StatistiqueAll.vue

--- src/components/StatistiqueAll.vue
+++ src/components/StatistiqueAll.vue
@@ -143,13 +143,13 @@ export default {
     async updateTable() {
         try {
             // Effectuer la première requête GET pour établir une session ou récupérer des données nécessaires
-            await axios.get('https://3538dde51d63156b011ebaa33232d05b.serveo.net/', {
+            await axios.get('back-csv-analyzer-production.up.railway.app/', {
                 withCredentials: true
             });
             this.errorMessage = ''; // Réinitialiser le message d'erreur
 
             // Effectuer la requête POST pour obtenir les entrées en fonction du type de statistique
-            const response = await axios.post('https://3538dde51d63156b011ebaa33232d05b.serveo.net/get_entries', {
+            const response = await axios.post('back-csv-analyzer-production.up.railway.app/get_entries', {
                 etat: this.statisticType, // Inclure le type de statistique dans la requête
             }, {
                 withCredentials: true // Inclure les cookies
